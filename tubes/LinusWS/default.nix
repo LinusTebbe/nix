b6504d0bd8c3aa952a4d67545d61110e13d4fdcf
LinusTebbe <linus@tebbe.dev>
2025-05-31 20:45:41 +0200

    enable generic scsi module

--- tubes/LinusWS/default.nix
+++ tubes/LinusWS/default.nix
@@ -16,7 +16,7 @@ in {
     ];
 
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
-  boot.kernelModules = ["kvm-amd"];
+  boot.kernelModules = ["kvm-amd" "sg"];
   boot.kernelPackages = pkgs.linuxPackages_6_13;
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   nixpkgs.hostPlatform = "x86_64-linux";
