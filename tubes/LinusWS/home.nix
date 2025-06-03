b9c0106a55b560718bd54e10f1e46dc133065abc
LinusTebbe <linus@tebbe.dev>
2025-06-03 19:36:54 +0200

    ADD git config

--- tubes/LinusWS/home.nix
+++ tubes/LinusWS/home.nix
@@ -1,103 +1,109 @@
 {
   home.stateVersion = "25.05";
 
-  programs.plasma = {
-    enable = true;
-    configFile = {
-      "kxkbrc"."Layout"."LayoutList" = "eu";
-      "kxkbrc"."Layout"."Use" = true;
+  programs = {
+    git = {
+      userEmail = "linus@tebbe.dev";
+      userName = "LinusTebbe";
     };
-    dataFile = {
-    };
-    workspace = {
-      clickItemTo = "select";
-      lookAndFeel = "org.kde.breezedark.desktop";
-      cursor = {
-        theme = "Breeze";
-        size = 20;
+    plasma = {
+      enable = true;
+      configFile = {
+        "kxkbrc"."Layout"."LayoutList" = "eu";
+        "kxkbrc"."Layout"."Use" = true;
       };
-    };
-    fonts = {
-      general = {
-        family = "JetBrains Mono";
-        pointSize = 10;
+      dataFile = {
       };
-    };
-    shortcuts = {
-      "services/org.kde.spectacle.desktop"."RectangularRegionScreenShot" = "Print";
-      "services/org.kde.spectacle.desktop"."_launch" = "Meta+Shift+S";
-    };
-    panels = [
-      {
-        location = "bottom";
-        screen = "all";
-        widgets = [
-          {
-            name = "org.kde.plasma.kickoff";
-            config = {
-              General = {
-                icon = "nix-snowflake-white";
-                alphaSort = true;
+      workspace = {
+        clickItemTo = "select";
+        lookAndFeel = "org.kde.breezedark.desktop";
+        cursor = {
+          theme = "Breeze";
+          size = 20;
+        };
+      };
+      fonts = {
+        general = {
+          family = "JetBrains Mono";
+          pointSize = 10;
+        };
+      };
+      shortcuts = {
+        "services/org.kde.spectacle.desktop"."RectangularRegionScreenShot" = "Print";
+        "services/org.kde.spectacle.desktop"."_launch" = "Meta+Shift+S";
+      };
+      panels = [
+        {
+          location = "bottom";
+          screen = "all";
+          widgets = [
+            {
+              name = "org.kde.plasma.kickoff";
+              config = {
+                General = {
+                  icon = "nix-snowflake-white";
+                  alphaSort = true;
+                };
               };
-            };
-          }
-          {
-            iconTasks = {
-              launchers = [
-                "applications:org.kde.dolphin.desktop"
-                "applications:org.kde.konsole.desktop"
-                "applications:google-chrome.desktop"
-              ];
-            };
-          }
-          {
-            systemTray.items = {
-              shown = [
-                "org.kde.plasma.bluetooth"
-                "org.kde.plasma.networkmanagement"
-                "org.kde.plasma.volume"
-              ];
-              hidden = [
-              ];
-            };
-          }
-          {
-            digitalClock = {
-              calendar = {
-                firstDayOfWeek = "monday";
-                showWeekNumbers = true;
+            }
+            {
+              iconTasks = {
+                launchers = [
+                  "applications:org.kde.dolphin.desktop"
+                  "applications:org.kde.konsole.desktop"
+                  "applications:google-chrome.desktop"
+                ];
               };
-              time = {
-                format = "24h";
-                showSeconds = "always";
+            }
+            {
+              systemTray.items = {
+                shown = [
+                  "org.kde.plasma.bluetooth"
+                  "org.kde.plasma.networkmanagement"
+                  "org.kde.plasma.volume"
+                ];
+                hidden = [
+                ];
               };
-              date.format.custom = "dd.MM.yyyy";
-            };
-          }
-        ];
-        hiding = "none";
-      }
-    ];
-    powerdevil = {
-      AC = {
-        powerButtonAction = "lockScreen";
-        autoSuspend.action = "nothing";
-        turnOffDisplay.idleTimeout = "never";
-        dimDisplay.enable = false;
-        powerProfile = "performance";
+            }
+            {
+              digitalClock = {
+                calendar = {
+                  firstDayOfWeek = "monday";
+                  showWeekNumbers = true;
+                };
+                time = {
+                  format = "24h";
+                  showSeconds = "always";
+                };
+                date.format.custom = "dd.MM.yyyy";
+              };
+            }
+          ];
+          hiding = "none";
+        }
+      ];
+      powerdevil = {
+        AC = {
+          powerButtonAction = "lockScreen";
+          autoSuspend.action = "nothing";
+          turnOffDisplay.idleTimeout = "never";
+          dimDisplay.enable = false;
+          powerProfile = "performance";
+        };
       };
-    };
-    session = {
-      sessionRestore.restoreOpenApplicationsOnLogin = "startWithEmptySession";
-    };
-    startup.startupScript = {
-      "start_discord" = {
-        text = "discord --start-minimized";
-        runAlways = true;
+      session = {
+        sessionRestore.restoreOpenApplicationsOnLogin = "startWithEmptySession";
       };
-      "start_spotify" = {
-        text = "spotify & xdotool search --sync --onlyvisible --class \"spotify\" windowminimize";
-        runAlways = true;
+      startup.startupScript = {
+        "start_discord" = {
+          text = "discord --start-minimized";
+          runAlways = true;
+        };
+        "start_spotify" = {
+          text = "spotify & xdotool search --sync --onlyvisible --class \"spotify\" windowminimize";
+          runAlways = true;
+        };
       };
     };
   };
