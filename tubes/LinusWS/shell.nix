6e58125fcc87fabc4191a16e454c9361e72495cd
LinusTebbe <linus@tebbe.dev>
2025-06-07 17:36:09 +0200

    ADD direnv

--- tubes/LinusWS/shell.nix
+++ tubes/LinusWS/shell.nix
@@ -2,6 +2,21 @@
   environment.systemPackages = [
     pkgs.direnv
   ];
+
+  programs.direnv = {
+    package = pkgs.direnv;
+    silent = true;
+    loadInNixShell = true;
+    direnvrcExtra = "";
+    nix-direnv = {
+      enable = true;
+      package = pkgs.nix-direnv;
+    };
+  };
+  environment.etc."direnv/direnv.toml".text = ''
+    [global]
+    hide_env_diff = true
+  '';
   programs.zsh = {
     enable = true;
     ohMyZsh = {
@@ -17,6 +32,8 @@
       "HIST_IGNORE_ALL_DUPS"
     ];
   };
+  users.defaultUserShell = pkgs.zsh;
+  system.userActivationScripts.zshrc = "touch .zshrc";
 
   programs.zsh.interactiveShellInit = ''eval "$(direnv hook zsh)"'';
 }
